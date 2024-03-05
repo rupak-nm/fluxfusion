c9daa6e493993fbae5da30863fd8d15304eb2f34
acharyarupak391 <acharyarupak391@gmail.com>
2024-03-05 12:21:15 +0545

    Completed setup with header & footer

---
+++ src/data/nav.ts
@@ -0,0 +1,36 @@
+const nav = [
+  {
+    name: "Solutions",
+    href: "/solutions",
+  },
+  {
+    name: "About Us",
+    href: "/about",
+  },
+  {
+    name: "Legal",
+    href: "/legal",
+  },
+  {
+    name: "Contact Us",
+    href: "/contact",
+    isButton: true,
+  },
+];
+
+const socials = [
+  {
+    icon: 'twitter-white',
+    link: 'https://www.twitter.com'
+  },
+  {
+    icon: 'linkedin-white',
+    link: 'https://www.linkedin.com'
+  },
+  {
+    icon: 'github-white',
+    link: 'https://www.github.com'
+  },
+]
+
+export { nav, socials };
